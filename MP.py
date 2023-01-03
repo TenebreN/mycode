1d97a6f79b753e0c3604e530c6c0d518f302da3a
TenebreN <vinnyj23@hotmail.com>
2023-01-03 23:13:32 +0000

    join and split

---
+++ MP.py
@@ -0,0 +1,14 @@
+import threading
+import time
+name= input("What is your name?")
+quest= input("What is your quest?")
+FTP= input("What is your FTP?")
+dow= input ("what is the day of the week?")
+time.sleep(5)
+print ("hello " + name,'!')
+print (quest + " is a valid quest!")
+print (FTP + " is weak.  Get stronger!")
+print ("Hello, " + name,'!' " happy " + dow,'!', sep='')
+
+
+
