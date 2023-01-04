f9bd1617d8b39666e78a7f74d7c7d8205ce013a0
TenebreN <vinnyj23@hotmail.com>
2023-01-04 17:53:19 +0000

    manipulating lists

---
+++ wednesdaychallenge.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+number= 2
+
+name= input("What is your name?\n>")
+
+# This is what you should see when print runs-
+# Hi <name>! Welcome to Day 2 of Python Training!
+
+#print(f"Hi ", {name.capitalize()} "! Welcome to Day ", {number} " of Python Training!")
+print(f"Hi {name.capitalize()} ! Welcome to Day {number} of Python Training!")
