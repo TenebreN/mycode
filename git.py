c11a4c45495a73ba912a56eca2b0849a4135d368
TenebreN <vinnyj23@hotmail.com>
2023-01-10 22:40:51 +0000

    latest update

--- git.py
+++ git.py
@@ -1,3 +1,17 @@
-git add *
-git commit -m "latest update"
-git push origin HEAD
+#!/usr/bin/env python3
+
+import os
+import time
+
+
+add = str("git add *")
+commit= str('git commit -m "latest update"')
+push = str("git push origin HEAD")
+
+os.system(f"{add}")
+time.sleep(1)
+os.system(f"{commit}")
+time.sleep(1)
+os.system(f"{push}")
+
+print("Success!")
